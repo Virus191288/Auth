956e7b5462eab6bc3504fb7423adaade800e568e
Virus191288 <Virus191288@gmail.com>
2015-10-05 11:01:08 +0500

    Create email.blade.php

---
+++ example/resources/views/auth/email.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.defaults')
+
+@section('content')
+
+    <div class="col-md-8 col-md-offset-2 form-content">
+        <h3 class="heading">please confirm your email</h3>
+    </div>
+
+@stop
