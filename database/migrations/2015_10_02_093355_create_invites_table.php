025a9bbc2cd1e745d615e342a82f55229a07ecff
s00d <Virus191288@gmail.com>
2015-10-16 12:31:28 +0500

    add invites
    table

---
+++ database/migrations/2015_10_02_093355_create_invites_table.php
@@ -0,0 +1,29 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateInvitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('invites');
+    }
+}
